3f9c0a3f27570ece999d4cbe3696d8ef98c82922
Tassiana Kautzmann <tassik@gmail.com>
2025-04-09 11:12:25 -0300

    check null pointer

--- mylib.c
+++ mylib.c
@@ -22,26 +22,34 @@ int criarTDA(ppTDA pp, int tamanho_info){
 
 
 int retornarTDA_info1(pTDA p, int *TDA_info1){
-    //1- pegar o valor de TDA_info1 que está na estrutura
+    //1- Vrificar se ponteiros não são nulos
+    if (!p || !TDA_info1) return FRACASSO; 
+
+    //2-pegar o valor de TDA_info1 que está na estrutura
     //e atribuir para onde TDA_info1 aponta (neste caso aponta para "retorno" que está no main) 
-    *TDA_info1 = p -> TDA_info1;
+    *TDA_info1 = p->TDA_info1;
 
     return SUCESSO;
+
 }
 
 
 int alterarTDA(pTDA p, int TDA_info){
+    //1- Vrificar se ponteiros não são nulos
+    if (!p) return FRACASSO;
+
     //1- a variável TDA_info1 da estrutura deve receber o valor passado por parâmetro em TDA_info
     //que neste caso é 10
     p->TDA_info1 = TDA_info;
 
     return SUCESSO;
+
 }
 
 
 int destroirTDA(ppTDA pp){
     //1- verifica se o ponteiro do ponteiro da estrutura não está vazio
-    if(*pp){
+    if(pp && *pp){
         //2- libera a memória de *pp
         free(*pp);
         //3- altera valor de *pp para NULL para evitar que contenha lixo
